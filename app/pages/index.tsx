a43626ea261ad769eb3f961b7b70c7c27dd41864
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-07-21 11:48:51 -0400

    Rework to show ISR. Note, ISR pages still go back to origin every time.
    Include config files that were hidden in previous commits.

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,5 +1,8 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
---
+++ app/pages/item/[id]/index.tsx
@@ -0,0 +1,28 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+import { useRouter } from "next/router";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const now = new Date();
+  return {
+    props: { timeString: `${now.toLocaleDateString()} ${now.toLocaleTimeString()}` },
+    revalidate: 20,
+  };
+};
+
+export default function ItemDetailPage({ timeString }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+  return (
+    <div className="flex justify-center mt-4">
+      <h2>
+        Item {router.query.id} {timeString}
+      </h2>
+    </div>
+  );
+}
