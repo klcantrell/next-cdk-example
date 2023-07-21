2c5993df6cd17320a35a0647bba4601dbfb6ea89
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-07-20 21:08:05 -0400

    Reworked example for deployment to AWS Lambda via CDK

--- app/app/item/[id]/page.tsx
+++ app/app/item/[id]/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useParams } from "next/navigation";
 
 export default function ItemDetailPage() {
-  const params = useSearchParams();
+  const params = useParams();
   return (
     <div className="flex justify-center mt-4">
-      <h2>Item {params.get('id')}</h2>
+      <h2>Item {params.id}</h2>
     </div>
   );
 }
